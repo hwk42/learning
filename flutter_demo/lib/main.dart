2a2bec3e86d26eebad0d486201f2901f4c35e302
hwk42 <hwk42@outlook.com>
2024-05-24 18:13:25 +0800

    first commit

---
+++ flutter_demo/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/getx_app.dart';
+import 'package:flutter_demo/hello.dart';
+import 'package:get/get.dart';
+
+import 'http_listview.dart';
+
+void main() => runApp(GetMaterialApp(home: Home(),debugShowCheckedModeBanner: false,));
